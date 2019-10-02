33de53682bb4c3d96ad9a504f4bca9f790f7c0d4
qianyi.qy <qianyi.qy@alibaba-inc.com>
2019-10-02 21:22:51 +0800

    Permute

---
+++ LeetCode/python/permute_1002.py
@@ -0,0 +1,22 @@
+
+def permute(nums):
+    res = []
+    visisted = set([])
+    def dfs(nums, path, visisted, res):
+        if len(nums) == len(path):
+            print("success ", path)
+            res.append(path+[])
+            return 
+        for i in range(len(nums)):
+            if i not in visisted:
+                visisted.add(i)
+                path.append(nums[i])
+                dfs(nums, path, visisted, res)
+                path.pop()
+                visisted.discard(i)
+    
+    dfs(nums, [], visisted, res)
+    return res
+
+
+print(permute([1,2,3]))
